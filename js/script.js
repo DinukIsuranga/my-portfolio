e8d3ad113eee48c6078c15e74e1aed5840807bac
DinukIsuranga <dinukliyanawanniarachchi2003@gmail.com>
2026-08-08 11:18:32 +0530

    Day 2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Typing Animation
+
+
+const typingText = document.getElementById("typing-text");
+
+const words = [
+    "ICT Undergraduate",
+    "Web Developer",
+    "Java Programmer",
+    "UI/UX Enthusiast",
+    "Software Engineering Student"
+];
