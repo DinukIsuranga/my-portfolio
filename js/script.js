0083150fafb7b4e8c7a2cb3af756a1532eab7c31
DinukIsuranga <dinukliyanawanniarachchi2003@gmail.com>
2026-08-08 18:40:35 +0530

    Day 2.1

--- js/script.js
+++ js/script.js
@@ -10,3 +10,69 @@ const words = [
     "UI/UX Enthusiast",
     "Software Engineering Student"
 ];
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function typeEffect() {
+
+    const currentWord = words[wordIndex];
+
+    if (!deleting) {
+
+        typingText.textContent = currentWord.substring(0, charIndex);
+        charIndex++;
+
+        if (charIndex > currentWord.length) {
+
+            deleting = true;
+
+            setTimeout(typeEffect, 1500);
+
+            return;
+        }
+
+    } else {
+
+        typingText.textContent = currentWord.substring(0, charIndex);
+        charIndex--;
+
+        if (charIndex < 0) {
+
+            deleting = false;
+            wordIndex++;
+
+            if (wordIndex >= words.length)
+                wordIndex = 0;
+
+            charIndex = 0;
+        }
+
+    }
+
+    setTimeout(typeEffect, deleting ? 50 : 100);
+
+}
+
+typeEffect();
+
+// Navbar Background on Scroll
+
+
+window.addEventListener("scroll", function () {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 60) {
+
+        navbar.style.background = "#0F172A";
+        navbar.style.transition = ".4s";
+
+    } else {
+
+        navbar.style.background = "rgba(15,23,42,.85)";
+
+    }
+
+});
